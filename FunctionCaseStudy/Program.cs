9426af78291be4f0ba0247fefb45b219640b6297
Dianara Kristy D. Garciano <142156759+mosshead19@users.noreply.github.com>
2024-09-18 13:09:45 +0800

    created program file

---
+++ FunctionCaseStudy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionCaseStudy
+{
+    internal class Program
+    {
+        //General Ave
+        //Letter Grade
+        //Student Report
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
