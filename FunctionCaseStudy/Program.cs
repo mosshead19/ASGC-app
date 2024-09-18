8c443268b2c0900a816bb6c307a6c3015b646d7b
CastleKen <202380191@psu.palawan.edu.phgit config --global user.name>
2024-09-18 22:12:58 +0800

    done

--- FunctionCaseStudy/Program.cs
+++ FunctionCaseStudy/Program.cs
@@ -9,7 +9,8 @@ namespace FunctionCaseStudy
     internal class Program
     {
         //Total Weighted Average
-        public static double totalAveScore(int[] quizScores, int[] assignmentScores, int finalExamScore) {
+        public static double totalAveScore(double[] quizScores, double[] assignmentScores, double finalExamScore)
+        {
 
             //average's grading criteria
             //Quiz = 25%
@@ -32,51 +33,121 @@ namespace FunctionCaseStudy
         }
 
         //Letter Grade
-        public static string letterGrade (double weightedAverageScore)
+        public static string letterGrade(double weightedAverageScore)
         {
-        //A: 90-100
-        //B: 80 - 89
-        //C: 70 - 79
-        //D: 60 - 69
-        //F: 0 - 59
+            //A: 90 - 100
+            //B: 80 - 89
+            //C: 70 - 79
+            //D: 60 - 69
+            //F: 0 - 59
 
             if (weightedAverageScore < 0 || weightedAverageScore > 100)
             {
                 return "Invalid score";
             }
 
-            else if (weightedAverageScore>=90)
+            else if (weightedAverageScore >= 90)
             {
                 return "A";
             }
-            else if (weightedAverageScore >= 80 )
+            else if (weightedAverageScore >= 80)
             {
-                return  "B";
+                return "B";
             }
-            else if (weightedAverageScore >= 70 )
+            else if (weightedAverageScore >= 70)
             {
                 return "C";
             }
-            else if (weightedAverageScore >= 60 )
+            else if (weightedAverageScore >= 60)
             {
                 return "D";
             }
-            else 
+            else
             {
                 return "F";
             }
-           
-            
+
+
         }
 
 
-        
-        
+
+
         //Student Report
+        public static void displayStudentReport(string studentName, double[] assignmentScores, double[] quizScores, double finalExamScore)
+        {
+
+            double weightedAverageScore = totalAveScore(quizScores, assignmentScores, finalExamScore);
+            string gradeLetter = letterGrade(weightedAverageScore);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("===========Student Report Summary===========");
+            Console.WriteLine();
+            Console.WriteLine($"Name of student: {studentName}");
+            Console.WriteLine("\nAssignment Scores:");
+
+            foreach (var i in assignmentScores)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine("\n\nQuiz Scores:");
+            foreach (var i in quizScores)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine($"\n\nFinal Exam Score: {finalExamScore}");
+            Console.WriteLine($"\nWeighted Average: {weightedAverageScore}");
+            Console.WriteLine($"\nLetter Grade: {gradeLetter}");
+        }
+
+
+
+
+
 
         static void Main(string[] args)
         {
-            //test case
+            Console.Write("Enter student's name: ");
+            string studentName = Console.ReadLine();
+
+
+
+            Console.Write("\nEnter the number of assignments: ");
+            int assNum = Convert.ToInt32(Console.ReadLine());
+
+            double[] assignmentScores = new double[assNum];
+
+            Console.WriteLine("Enter the assignment scores: ");
+            for (int i = 0; i <= assNum - 1; i++)
+            {
+                assignmentScores[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+
+            Console.Write("\nEnter the number of quizzes: ");
+            int quizNum = Convert.ToInt32(Console.ReadLine());
+
+            double[] quizScores = new double[quizNum];
+
+            Console.WriteLine("Enter the quizz scores: ");
+            for (int i = 0; i <= quizNum - 1; i++)
+            {
+                quizScores[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+
+            Console.Write("\nEnter final exam score: ");
+            double finalExamScore = Convert.ToDouble(Console.ReadLine());
+
+
+            displayStudentReport(studentName, assignmentScores, quizScores, finalExamScore);
         }
     }
 }
