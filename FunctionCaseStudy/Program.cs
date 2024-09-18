f9a3030dbd39bbc205d9ce164d363b84c571384b
Dianara Kristy D. Garciano <142156759+mosshead19@users.noreply.github.com>
2024-09-18 13:54:37 +0800

    Update Program.cs

--- FunctionCaseStudy/Program.cs
+++ FunctionCaseStudy/Program.cs
@@ -9,6 +9,18 @@ namespace FunctionCaseStudy
     internal class Program
     {
         //General Ave
+           //public static int totalAve(int[] quizScores, int[] assignmentScores, int finalExamScore){
+                //sum quiz scores
+                //sum assingmentScores
+                //Quiz = 25%
+                //Assignment = 25%
+                //Final Exam = 50%
+                //Calculate Ave
+         
+        
+          // }
+
+        
         //Letter Grade
         //Student Report
 
